5176ad4bc56f574ab60cd7c1669324af07f77f41
Yizhou <zyz0205@hotmail.com>
2018-12-07 04:20:00 -0800

    add interface for age estimation

--- demo.py
+++ demo.py
@@ -74,7 +74,68 @@ def yield_images_from_dir(image_dir):
             yield cv2.resize(img, (int(w * r), int(h * r)))
 
 
-def main():
+def age_estimation_with_draw():
+    args = get_args()
+    depth = args.depth
+    k = args.width
+    weight_file = args.weight_file
+    margin = args.margin
+    image_dir = args.image_dir
+
+    if not weight_file:
+        weight_file = get_file("weights.28-3.73.hdf5", pretrained_model, cache_subdir="pretrained_models",
+                               file_hash=modhash, cache_dir=Path(__file__).resolve().parent)
+
+    # for face detection
+    detector = dlib.get_frontal_face_detector()
+    # load model and weights
+    img_size = 64
+    model = WideResNet(img_size, depth=depth, k=k)()
+    model.load_weights(weight_file)
+
+    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+
+    idx = 0
+    for img in image_generator:
+        idx += 1
+        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_h, img_w, _ = np.shape(input_img)
+
+        # detect faces using dlib detector
+        detected = detector(input_img, 1)
+        faces = np.empty((len(detected), img_size, img_size, 3))
+        print("detected {}".format(len(detected)))
+        if len(detected) > 0:
+            for i, d in enumerate(detected):
+                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                xw1 = max(int(x1 - margin * w), 0)
+                yw1 = max(int(y1 - margin * h), 0)
+                xw2 = min(int(x2 + margin * w), img_w - 1)
+                yw2 = min(int(y2 + margin * h), img_h - 1)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+
+            # predict ages and genders of the detected faces
+            results = model.predict(faces)
+            predicted_genders = results[0]
+            ages = np.arange(0, 101).reshape(101, 1)
+            predicted_ages = results[1].dot(ages).flatten()
+            print(predicted_ages)
+            # draw results
+            for i, d in enumerate(detected):
+                label = "{}, {}".format(int(predicted_ages[i]),
+                                        "F" if predicted_genders[i][0] > 0.5 else "M")
+                draw_label(img, (d.left(), d.top()), label)
+
+        cv2.imwrite('./new_result'+str(idx)+'.jpg', img)
+        key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
+
+        if key == 27:  # ESC
+            break
+
+
+def age_estimation(image_list):
     args = get_args()
     depth = args.depth
     k = args.width
@@ -94,16 +155,14 @@ def main():
     model = WideResNet(img_size, depth=depth, k=k)()
     model.load_weights(weight_file)
 
-    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
-
-    for img in image_generator:
-        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_h, img_w, _ = np.shape(input_img)
-
+    image_generator = yield_images_from_dir(image_dir)
+    for image in image_list:
+        img_h, img_w, _ = np.shape(image)
         # detect faces using dlib detector
-        detected = detector(input_img, 1)
+        detected = detector(image, 1)
         faces = np.empty((len(detected), img_size, img_size, 3))
-
+        bgr_image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)        
+        
         if len(detected) > 0:
             for i, d in enumerate(detected):
                 x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
@@ -111,28 +170,36 @@ def main():
                 yw1 = max(int(y1 - margin * h), 0)
                 xw2 = min(int(x2 + margin * w), img_w - 1)
                 yw2 = min(int(y2 + margin * h), img_h - 1)
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
-
+                cv2.rectangle(bgr_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                faces[i, :, :, :] = cv2.resize(bgr_image[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
             # predict ages and genders of the detected faces
             results = model.predict(faces)
             predicted_genders = results[0]
             ages = np.arange(0, 101).reshape(101, 1)
             predicted_ages = results[1].dot(ages).flatten()
-
-            # draw results
-            for i, d in enumerate(detected):
-                label = "{}, {}".format(int(predicted_ages[i]),
-                                        "F" if predicted_genders[i][0] > 0.5 else "M")
-                draw_label(img, (d.left(), d.top()), label)
-
-        cv2.imshow("result", img)
-        key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
-
-        if key == 27:  # ESC
-            break
+            print(predicted_ages)
+            print("Left: {} Top: {} Right: {} Bottom: {}".format(d.left(), d.top(), d.right(), d.bottom()))
 
 
 if __name__ == '__main__':
-    main()
+    
+    args = get_args()
+    image_dir = args.image_dir
+    image_dir = Path(image_dir)
+
+    image_list = []    
+    for image_path in image_dir.glob("*.*"):
+        img = cv2.imread(str(image_path), 1)
+
+        if img is not None:
+            h, w, _ = img.shape
+            r = 640 / max(w, h)
+            img = cv2.resize(img, (int(w * r), int(h * r)))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            image_list.append(img)
+
+    print(len(image_list))
+    age_estimation(image_list)
+    '''    
+    age_estimation_with_draw()
+    '''
